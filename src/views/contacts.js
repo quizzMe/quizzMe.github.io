e5b48a0aac3af288baadee15976e5fe7681f18f6
Doychin Ivanov <doychinivn@gmail.com>
2021-04-13 00:23:36 +0300

    updated contacts page

--- src/views/contacts.js
+++ src/views/contacts.js
@@ -1,12 +1,42 @@
 import { html } from '../libraries.js';
 
 const contactPageTemplate = () => html `
-        <div class="glass" id="about-container">
-            <div id="info-div">
-                <p>lorem impsum</p>
-            </div>
+    <div class="glass" id="contacts-container">
+        <div id="contact-div">
+            <ul class="social-links">
+                <li class="facebook">
+                    <a href="#">
+                         <i class="fab fa-facebook-square"></i>
+                     </a>
+                </li>
 
-         </div>
+                <li class="twitter">
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+
+                <li class="google">
+                    <a href="#">
+                        <i class="fab fa-google-plus"></i>
+                    </a>
+                </li>
+
+                <li class="linkedin">
+                    <a href="#">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </li>
+
+                <li class="instagram">
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
 `;
 
 export function contactPage(ctx){
