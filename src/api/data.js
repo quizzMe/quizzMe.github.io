06824092b41c177b1cf63cd04987f63f7b294ecc
Doychin Ivanov <doychinivn@gmail.com>
2021-04-14 16:46:34 +0300

    updated api

--- src/api/data.js
+++ src/api/data.js
@@ -8,6 +8,14 @@ export const register = api.register;
 export const logout = api.logout;
 
 //Implement application spesific requests
+export async function getQuizzes(){
+    return await api.get(host + '/classes/Quiz');
+}
+
+export async function getQuizById(id){
+    return await api.get(host + '/classes/Quiz/' + id);
+}
+
 export async function createQuiz(quiz){
     return await api.post(host + '/classes/Quiz', quiz)
 }
