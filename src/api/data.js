904e97a1306fc97fbd72e03db42e9a9b27cde707
Doychin Ivanov <doychinivn@gmail.com>
2021-04-14 17:05:23 +0300

    updated api

--- src/api/data.js
+++ src/api/data.js
@@ -8,14 +8,31 @@ export const register = api.register;
 export const logout = api.logout;
 
 //Implement application spesific requests
-export async function getQuizzes(){
+export async function getQuizzes() {
     return await api.get(host + '/classes/Quiz');
 }
 
-export async function getQuizById(id){
+export async function getQuizById(id) {
     return await api.get(host + '/classes/Quiz/' + id);
 }
 
-export async function createQuiz(quiz){
-    return await api.post(host + '/classes/Quiz', quiz)
+export async function updateQuiz(id, quiz) {
+    return await api.put(host + '/classes/Quiz/' + id, quiz);
+}
+
+export async function deleteQuiz(id) {
+    return await api.del(host + '/classes/Quiz/' + id);
+}
+
+export async function createQuiz(quiz) {
+    const userId = sessionStorage.getItem('userId');
+    const body = Object.assign({}, quiz, {
+        owner: {
+            __type: 'Pointer',
+            className: '_User',
+            objectId: userId
+        }
+    })
+
+    return await api.post(host + '/classes/Quiz', body);
 }
