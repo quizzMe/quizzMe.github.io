857ce485d5747002cea259de04684c9872de8724
Doychin Ivanov <doychinivn@gmail.com>
2021-04-20 13:49:53 +0300

    update progress

--- src/views/quiz/quiz.js
+++ src/views/quiz/quiz.js
@@ -2,63 +2,63 @@ import { html, until } from '../../libraries.js';
 import { getQuizById, getQuestionsByQuizId } from '../../api/data.js';
 import { spinner } from '../../common/loaders.js';
 
-const quizTemplate = (quiz, questions) => html`
+const quizTemplate = (quiz, questions, currentIndex) => html`
 <section id="quiz" class="glass common">
     <header id="quiz-navigation" class="edit-create-title">
         <h1>${quiz.title}</h1>
-        <h2>Question 1 / ${questions.length}</h2>
+        <h2>Question ${currentIndex + 1} / ${questions.length}</h2>
         <nav class="quiestions-banner">
             <span class="block">Question index</span>
-            ${questions.map((q, i) => html`<a class="q-index q-current q-answered" href="/quiz/${quiz.objectId}?question=${i}"></a>`)}
+            ${questions.map((q, i) => html`<a class="q-index q-current q-answered"
+                href="/quiz/${quiz.objectId}?question=${i}"></a>`)}
         </nav>
     </header>
     <div class="question-template">
 
         <article class="question">
             <p class="q-text">
-                This is the first question. Veniam unde beatae est ab quisquam quos officia, eius
-                harum accusamus adipisci?
+                ${questions[currentIndex].text}
             </p>
 
-        <form>
-            <div data-index='question-1' class="option-holder">
-                    <div class="editor-input">
-                        <label class="radio">
-                            <input class="radio-input" type="radio" name="question-1" value="0" />
-                            <div class="radio-radio"></div>
-                        </label>
-                        <span>This is answer 1</span>
+            <form>
 
-                    </div>
-                    <div class="editor-input">
-                        <label class="radio">
-                            <input class="radio-input" type="radio" name="question-1" value="0" />
-                            <div class="radio-radio"></div>
-                        </label>
-                        <span>This is answer 2</span>
-                    </div>
-                    <div class="editor-input">
-                        <label class="radio">
-                            <input class="radio-input" type="radio" name="question-1" value="0" />
-                            <div class="radio-radio"></div>
-                        </label>
-                        <span>This is answer 3</span>
-                    </div>
-        </form>
+                ${questions.map((q,i) => questionTemplate(q, i, i==currentIndex))}
 
-                <nav class="q-control">
-                    <span class="block question-progress">12 questions remaining</span>
-                    <a class="add-answer-btn common choose" href=#><i class="fas fa-arrow-left"></i> Previous</a>
-                    <a class="add-answer-btn common choose" href=#><i class="fas fa-sync-alt"></i> Start over</a>
-                    <div class="right-col">
-                        <a class="add-answer-btn common choose" href=#>Next <i class="fas fa-arrow-right"></i></a>
-                        <a class="add-answer-btn common choose" href=#>Submit answers</a>
-                    </div>
-                </nav>
+            </form>
+
+            <nav class="q-control">
+                <span class="block question-progress">12 questions remaining</span>
+                <a class="add-answer-btn common choose" href=#><i class="fas fa-arrow-left"></i> Previous</a>
+                <a class="add-answer-btn common choose" href=#><i class="fas fa-sync-alt"></i> Start over</a>
+                <div class="right-col">
+                    <a class="add-answer-btn common choose" href=#>Next <i class="fas fa-arrow-right"></i></a>
+                    <a class="add-answer-btn common choose" href=#>Submit answers</a>
+                </div>
+            </nav>
         </article>
 
     </div>
 </section>
+`;
+
+
+const questionTemplate = (question, index, isCurrent) => html`
+<div data-index='question-${index}' class="option-holder" style=${isCurrent ? '' : 'display:none' } >
+
+    ${question.answers.map((a, i) => answerTemplate(index, i, a))}
+
+</div>
+`;
+
+
+const answerTemplate = (questionIndex, index, value) => html`
+    <div class="editor-input">
+        <label class="radio">
+            <input class="radio-input" type="radio" name="question-${questionIndex}" value=${index} />
+            <div class="radio-radio"></div>
+        </label>
+        <span>${value}</span>
+    </div>
 `;
 
 export async function quizPage(ctx) {
@@ -66,10 +66,10 @@ export async function quizPage(ctx) {
     ctx.render(until(getQuiz(quizId), spinner()));
 }
 
-async function getQuiz(quizId){
+async function getQuiz(quizId) {
     const quiz = await getQuizById(quizId);
     const ownerId = quiz.owner.objectId;
     const questions = await getQuestionsByQuizId(quizId, ownerId);
 
-    return quizTemplate(quiz, questions);
+    return quizTemplate(quiz, questions, 0);
 }
