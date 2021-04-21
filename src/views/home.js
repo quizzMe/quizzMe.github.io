ead88ba0353a75cee8f8addd9ba10ef7d1dafc82
Doychin Ivanov <doychinivn@gmail.com>
2021-04-21 12:45:17 +0300

    only author can edit quiz

--- src/views/home.js
+++ src/views/home.js
@@ -84,6 +84,9 @@ const quizRecentTemplate = (quiz) => html`
         </div>
 
         <div class="view-quiz">
+        ${ quiz.owner.objectId == sessionStorage.getItem('userId') ? 
+        html`<a class="common choose" href=${'/edit/' + quiz.objectId}>Edit Quiz</a>` 
+        : ''}
             <a class="common choose" href=${'/details/' + quiz.objectId}>View Quiz</a>
         </div>
 </article>
