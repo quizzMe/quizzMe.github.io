a9168d132d77f9f3dda58a6435f9b5a78e566306
Doychin Ivanov <doychinivn@gmail.com>
2021-04-21 19:59:11 +0300

    profile page progress

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,16 +1,41 @@
-import { html } from '../libraries.js';
+import { html, styleMap } from '../libraries.js';
 import { getQuizzes, getUserById } from '../api/data.js';
 
-const profileTemplate = (userId, quizzes, visitorIsOwner, userProfile) => html`
+const profileTemplate = ( quizzes, visitorIsOwner, userProfile) => html`
     <div id="profile-page" class="glass common">
 
-        ${visitorIsOwner ? 
-        html`<h1>Hello, ${sessionStorage.getItem('username')}</h1>` 
-        : '' }
+        <div class="content-preview">
+             ${visitorIsOwner ? 
+                html`<h1>Hello, ${sessionStorage.getItem('username')}</h1>` 
+                : '' }
 
-        <h2>${visitorIsOwner ? 'You have' : `${userProfile} has`} ${quizzes.length} quiz${quizzes.length > 1 ? 'zes' : ''}:</h2>
+            <h2>${visitorIsOwner ? 'You have' : `${userProfile} has`} ${quizzes.length} quiz${quizzes.length > 1 ? 'zes' : ''}:</h2>
         
+             ${quizzes.map(quizzTemplates)}
+        </div>
     </div>
+`;
+
+
+const quizzTemplates = (quiz) => html`
+<article class="quiz-preview">
+    <div class="quiz-description">
+        <h3><a class="common" href=${'/details/' + quiz.objectId}>${quiz.title}</a></h3>
+        <span class="quiz-topic">Topic: ${quiz.topic}</span>
+        <div class="quiz-meta">
+            <span>${quiz.questionCount} question${quiz.questionCount == 1 ? '' : 's'}</span>
+            <span>|</span>
+            <span>Taken ? times</span>
+            </div>
+        </div>
+
+        <div class="view-quiz">
+        ${ quiz.owner.objectId == sessionStorage.getItem('userId') ? 
+        html`<a class="common choose" href=${'/edit/' + quiz.objectId}>Edit Quiz</a>` 
+        : ''}
+            <a class="common choose" href=${'/details/' + quiz.objectId}>View Quiz</a>
+        </div>
+</article>
 `;
 
 export async function profilePage(ctx){
@@ -19,5 +44,5 @@ export async function profilePage(ctx){
     const userQuizzes = allQuizzes.filter(x => x.owner.objectId == userId);
     const userProfile = await getUserById(userId);
 
-    ctx.render(profileTemplate(userId, userQuizzes, userId == sessionStorage.getItem('userId'), userProfile));
+    ctx.render(profileTemplate(userQuizzes, userId == sessionStorage.getItem('userId'), userProfile));
 }
