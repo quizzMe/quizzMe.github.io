dc28465bcf2740a4f5b383df35a79c416a922a39
Doychin Ivanov <doychinivn@gmail.com>
2021-04-22 16:34:38 +0300

    progress update

--- src/views/quiz/results.js
+++ src/views/quiz/results.js
@@ -2,25 +2,41 @@ import { html } from '../../libraries.js';
 
 const resultTemplate = (quiz, result) => html`
 <section id="summary" class="glass common">
-    <div>
         <article class="details">
             <h1>Quiz Results</h1>
             <h2>${quiz.title}</h2>
             <h2>A quiz by <a class="blink" data-micron="fade" data-micron-duration="2" href=${`/profile/${quiz.owner.objectId}`}>${quiz.owner.username}</a></h2>
 
-            <div class="summary summary-top">
+            <div class="summary-top">
                 ${result.percent}%
             </div>
 
-            <div class="summary">
+            <div class="summary-bottom">
             ${result.correct}/${result.total} correct answers
             </div>
 
-            <a class="action cta" href=${`/quiz/${quiz.objectId}`}><i class="fas fa-sync-alt"></i> Retake Quiz</a>
-            <a class="action cta" href="#"><i class="fas fa-clipboard-list"></i> See Details</a>
-
+            <div class="btn-holderr">
+                <a class="add-answer-btn common choose" href=${`/quiz/${quiz.objectId}`}><i class="fas fa-sync-alt"></i> Retake Quiz</a>
+                <a class="add-answer-btn common choose" href="javascript:void(0)"><i class="fas fa-clipboard-list"></i> See Details</a>
+            </div>
         </article>
-    </div>
+
+        <article class="quiz-preview result-holder">
+            <div class="quiz-description">
+            <h3> Question 1</h3>
+            <span class="quiz-topic">This is the question</span>
+            <i class="fas fa-times"></i>
+            <i class="fas fa-check"></i>
+        </article>
+
+        <article class="quiz-preview result-holder">
+            <div class="quiz-description">
+            <h3> Question 1</h3>
+            <span class="quiz-topic">This is the question</span>
+            <i class="fas fa-times"></i>
+            <i class="fas fa-check"></i>
+        </article>
+</section>
 `;
 
 
