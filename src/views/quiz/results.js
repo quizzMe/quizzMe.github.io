fbcb952812f569b38db88df4550e6668a359a7c8
Doychin Ivanov <doychinivn@gmail.com>
2021-04-22 15:49:39 +0300

    progress update

--- src/views/quiz/results.js
+++ src/views/quiz/results.js
@@ -1,21 +1,21 @@
 import { html } from '../../libraries.js';
 
-const resultTemplate = () => html`
+const resultTemplate = (quiz, result) => html`
 <section id="summary">
     <div class="hero layout">
         <article class="details glass">
             <h1>Quiz Results</h1>
-            <h2>Extensible Markup Language</h2>
+            <h2>${quiz.title}</h2>
 
             <div class="summary summary-top">
-                85%
+                ${result.percent}%
             </div>
 
             <div class="summary">
-                12/15 correct answers
+            ${result.correct}/${result.total} correct answers
             </div>
 
-            <a class="action cta" href="#"><i class="fas fa-sync-alt"></i> Retake Quiz</a>
+            <a class="action cta" href=${`/quiz/${quiz.objectId}`}><i class="fas fa-sync-alt"></i> Retake Quiz</a>
             <a class="action cta" href="#"><i class="fas fa-clipboard-list"></i> See Details</a>
 
         </article>
@@ -24,5 +24,13 @@ const resultTemplate = () => html`
 
 
 export async function resultPage(ctx){
-    ctx.render(resultTemplate());
+    const questions = ctx.quiz.questions;
+    const answers = ctx.quiz.answers;
+    const correct = answers.reduce((r, c, i) => r + Number(questions[i].correctIndex == c), 0);
+    
+    ctx.render(resultTemplate(ctx.quiz, {
+        percent: (correct / questions.length * 100).toFixed(0),
+        correct,
+        total: questions.length
+    }));
 }
