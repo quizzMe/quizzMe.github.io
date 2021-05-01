63c14d94ae23332687a786bd5b9d0d42910a941a
Doychin Ivanov <doychinivn@gmail.com>
2021-05-01 13:36:05 +0300

    about page content done

--- src/views/about.js
+++ src/views/about.js
@@ -2,8 +2,10 @@ import { html } from '../libraries.js';
 
 const aboutPageTemplate = () => html `
         <div class="glass" id="about-container">
-            <div id="info-div">
-                <p>lorem impsum</p>
+            <div id="info-div" class="common">
+                <p>QuizzMe is a SPA (Single Page Application) made as a school project by web developer ${html`<a href="https://github.com/doychinivanov" target="_blank">Doychin Ivanov</a>`} as a way to practice and master his skills currently gained at ${html`<a href="https://softuni.bg/" target="_blank">SoftUni</a>`}.
+                    The project aims to practice consepts like Asynchronous Programming, using a remote server (BaaS), CRUD operations, User Authentication, Client Side Rendering (Templating Concepts), Routing, Component Approach and others. More about the project can be found on QuizzMe's ${html`<a href="https://github.com/quizzMe" target="_blank">github page</a>`}. 
+                </p>
             </div>
 
          </div>
